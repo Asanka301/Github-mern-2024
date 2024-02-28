ad78bd10b9e1920810de172202eadbfb5d5392fa
Asanka301 <asankagamage301@gmail.com>
2024-02-28 19:28:17 +0530

    api routes set

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -15,22 +15,14 @@ const HomePage = () => {
 
   const getUserProfileAndRepos = useCallback(async (username = "Asanka301") => {
     try {
-      const userRes = await fetch(`https://api.github.com/users/${username}`, {
-        headers: {
-          authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
-        },
-      });
-      const userProfile = await userRes.json();
-      setUserProfile(userProfile);
-
-      const repoRes = await fetch(userProfile.repos_url);
-      const repos = await repoRes.json();
-
+      const res = await fetch(
+        `http://localhost:5000/api/users/profile/${username}`
+      );
+      const { repos, userProfile } = await res.json();
       repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
       setRepos(repos);
-      console.log("userProfile:", userProfile);
-      console.log("repos:", repos);
+      setUserProfile(userProfile);
       return { userProfile, repos };
     } catch (error) {
       toast.error(error.message);
