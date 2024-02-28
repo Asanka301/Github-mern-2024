da07a5f192a96f9e73a554a6d288fc78b55b0d6b
Asanka301 <asankagamage301@gmail.com>
2024-02-28 22:25:15 +0530

    server endpoints added

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -6,24 +6,24 @@ import {
 } from "react-icons/ri";
 import { FaXTwitter } from "react-icons/fa6";
 import { TfiThought } from "react-icons/tfi";
-import { formatMemberSince } from "../utils/functions";
 import { FaEye } from "react-icons/fa";
+import { formatMemberSince } from "../utils/functions";
+import LikeProfile from "./LikeProfile";
 
 const ProfileInfo = ({ userProfile }) => {
   // const userProfile = {
-  //   avatar_url:
-  //     "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
-  //   bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
-  //   email: "johndoe@gmail.com",
-  //   followers: 100,
-  //   following: 200,
-  //   html_url: "https://github.com/Asanka301",
-  //   location: "Somewhere, Earth",
-  //   name: "John Doe",
-  //   public_gists: 100,
-  //   public_repos: 100,
-  //   twitter_username: "Asanka",
-  //   login: "johndoe",
+  // 	avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
+  // 	bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
+  // 	email: "johndoe@gmail.com",
+  // 	followers: 100,
+  // 	following: 200,
+  // 	html_url: "https://github.com/burakorkmez",
+  // 	location: "Somewhere, Earth",
+  // 	name: "John Doe",
+  // 	public_gists: 100,
+  // 	public_repos: 100,
+  // 	twitter_username: "johndoe",
+  // 	login: "johndoe",
   // };
 
   const memberSince = formatMemberSince(userProfile?.created_at);
@@ -31,7 +31,7 @@ const ProfileInfo = ({ userProfile }) => {
   return (
     <div className="lg:w-1/3 w-full flex flex-col gap-2 lg:sticky md:top-10">
       <div className="bg-glass rounded-lg p-4">
-        <div className="flex gap-4 items-center">
+        <div className="flex gap-3 items-center">
           {/* User Avatar */}
           <a href={userProfile?.html_url} target="_blank" rel="noreferrer">
             <img
@@ -41,7 +41,9 @@ const ProfileInfo = ({ userProfile }) => {
             />
           </a>
           {/* View on Github */}
+
           <div className="flex gap-2 items-center flex-col">
+            <LikeProfile userProfile={userProfile} />
             <a
               href={userProfile?.html_url}
               target="_blank"
@@ -140,5 +142,4 @@ const ProfileInfo = ({ userProfile }) => {
     </div>
   );
 };
-
 export default ProfileInfo;
